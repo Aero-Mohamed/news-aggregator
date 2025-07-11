5ad84f9e9266c3b5bdcdc39da386cb4b49461c9f
Mohamed Ahmed <aero.mohamed@icloud.com>
2025-07-11 22:45:59 +0300

    dockerize backend and install api passport

---
+++ backend/init.sh
@@ -0,0 +1,23 @@
+# backend/init.sh
+#!/bin/bash
+
+echo "🔧 Running Laravel backend initialization..."
+
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+if [ -f artisan ]; then
+
+    php -r "file_exists('.env') || copy('.env.example', '.env');"
+
+    php artisan migrate --force
+
+    php artisan install:api --passport -q
+    php artisan passport:keys -n -q
+
+    php artisan storage:link
+    php artisan optimize
+    php artisan cache:clear
+    php artisan route:clear
+fi
+
+echo "✅ Laravel backend is initialized!"
